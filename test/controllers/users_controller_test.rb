bf60ec97716894031455a4bd81a809759ad0dc5b
AleBard <alessandro.bardini93@gmail.com>
2019-08-17 17:07:15 +0200

    Edit profile page requires logged users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,28 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "should get new" do
     get signup_url
     assert_response :success
   end
 
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name:  "Michael",
+                                              email: "foo@example.com",
+                                              password:              "foobar",
+                                              password_confirmation: "foobar" } }
+    assert_not flash.empty?
+    assert_redirected_to login_path
+  end
 end
