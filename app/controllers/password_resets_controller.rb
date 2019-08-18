6513a94bd2fee8171ab42394004095f2d161ce6d
AleBard <alessandro.bardini93@gmail.com>
2019-08-18 20:50:09 +0200

    Reset edit action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,9 +1,11 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
   end
 
   def create
-    @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
@@ -16,5 +18,22 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+    if valid_user
+      # update password
+    else
+      # something wrong
+    end
   end
+
+  private
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user.
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
 end
