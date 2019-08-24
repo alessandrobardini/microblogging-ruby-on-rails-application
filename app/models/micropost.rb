42f737a660e242f895146518e4e8df5876e96d72
AleBard <alessandro.bardini93@gmail.com>
2019-08-24 14:44:34 +0200

    Image resize

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,12 +4,14 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validates :picture_size
+  validate  :picture_size
 
-  # Validates the size of an uploaded picture.
-  def picture_size
-    if picture.size > 5.megabytes
-      errors.add(:picture, "should be less than 5MB")
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
     end
-  end
 end
